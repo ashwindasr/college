93f622a5f50fc37ea5d6b95ef3f93e569a995a89
Ashwin Das <ashwindasr@users.noreply.github.com>
2018-10-24 20:33:18 +0530

    Create sqroot.asm

---
+++ mpc_lab/sqroot.asm
@@ -0,0 +1,25 @@
+ASSUME CS:CODE,DS:DATA
+DATA SEGMENT
+  num db 05h
+DATA ENDS
+CODE SEGMENT
+START: MOV AX,DATA
+       MOV DS,AX
+       MOV BL,num
+       MOV CL,01H
+  L:   MOV AL,CL
+       MUL CL
+       CMP AL,BL
+       JE ANS
+       INC CL
+       CMP AL,BL
+       JL L
+
+       MOV AH,4CH
+       INT 21H
+
+ ANS:  MOV DL,CL
+       MOV AH,4CH
+       INT 21H
+CODE ENDS
+END START

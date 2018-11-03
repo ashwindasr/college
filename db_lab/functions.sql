be87a09e65910c29f876f95e8c7e4c6c2a41ac43
Ashwin Das <ashwindasr@users.noreply.github.com>
2018-11-03 16:05:28 +0530

    Update functions.sql

--- db_lab/functions.sql
+++ db_lab/functions.sql
@@ -63,3 +63,89 @@ BEGIN
 END;
 $BODY$
 LANGUAGE plpgsql;
+
+-- Function to list students with strength grester than n
+CREATE OR REPLACE FUNCTION dispp(n INT)
+RETURNS TABLE(
+  id INT,
+  stuame VARCHAR,
+  divis VARCHAR,
+  s_cnt INT
+  ) AS
+$BODY$
+BEGIN
+    RETURN QUERY SELECT * FROM class_ WHERE st_cnt>n;
+END;
+$BODY$
+LANGUAGE plpgsql;
+
+-- Query to list students with fname like Aiswarya
+CREATE OR REPLACE FUNCTION dispn(s VARCHAR)
+RETURNS TABLE(
+  id INT,
+  fname VARCHAR,
+  lname VARCHAR,
+  c_id INT  
+  )
+  AS
+  $$
+  BEGIN
+    RETURN QUERY SELECT * FROM student WHERE st_fname like s;
+  END;
+  $$
+  LANGUAGE plpgsql;
+  
+  --Query to list the name and class
+  CREATE OR REPLACE FUNCTION dispn2(s VARCHAR)
+RETURNS TABLE(
+  name VARCHAR,
+  div VARCHAR
+  )
+  AS
+  $$
+  BEGIN
+    RETURN QUERY SELECT st_fname,division FROM student NATURAL JOIN class_ WHERE st_fname like s;
+  END;
+  $$
+  LANGUAGE plpgsql;
+  
+  
+  CREATE OR REPLACE FUNCTION testcur()
+  RETURNS VOID AS
+  $$
+  DECLARE
+    cur CURSOR FOR SELECT st_fname, st_lname FROM student LIMIT 5;
+  BEGIN
+    FOR rec in cur
+    LOOP 
+        RAISE NOTICE 'fname: %,lname: %', rec.st_fname, rec.st_lname;
+    END LOOP;
+  END;
+  $$
+  LANGUAGE plpgsql;
+  
+  
+  CREATE OR REPLACE FUNCTION maptr(
+    cname VARCHAR,
+    cdiv VARCHAR,
+    tfname VARCHAR,
+    tlname VARCHAR
+    )
+    AS
+    $$
+    DECLARE
+       c_id VARCHAR;
+       t_id VARCHAR;
+       cur CURSOR FOR SELECT st_id FROM student NATURAL JOIN class_ WHERE class_name LIKE 'Ten' AND division LIKE 'C';
+    BEGIN
+      SELECT class_id FROM class_
+      WHERE class_name LIKE 'Ten' AND division LIKE 'C' INTO c_id;
+      SELECT tr_id FROM teacher 
+      WHERE tr_fname LIKE 'Linda' AND tr_lname LIKE 'Max' INTO t_id;
+      FOR rec IN cur
+      LOOP
+          INSERT INTO stud_class VALUES(rec.st_id,c_id,t_id);
+      END LOOP;
+    END;
+    $$
+    LANGUAGE plpgsql;

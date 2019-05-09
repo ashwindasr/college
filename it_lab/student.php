23c135810bbf0ea649d0996a5920983296a070af
Ashwin Das <ashwindasr@users.noreply.github.com>
2019-05-09 15:19:51 +0530

    Update student.php

--- it_lab/student.php
+++ it_lab/student.php
@@ -4,20 +4,27 @@
 	$rollno = $_POST["rollno"];
 	$branch = $_POST["branch"];
 
+
 	$db = "localhost";
 	$username = "root";
 	$password = "root";
 	
-	$conn = mysqli_connect($db, $username, $password,"registration");
+	$conn = mysqli_connect($db, $username, $password,"test");
+
+
 	
-	if(!mysqli_query($conn, "insert into registration values($name, $rollno, $branch)")){
-		echo mysqli_error();
+	$sql = "INSERT INTO registration values('$rollno','$name','$branch');";
+	
+	if(!mysqli_query($conn, $sql)){
+		echo 'error';
 	}
 	else{
 		echo 'success';
-}
+	}
 	mysqli_close($conn);
 		
 		
 
 ?>
+
+
